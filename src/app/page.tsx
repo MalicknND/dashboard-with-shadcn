7d87c10930886031a191f6098536aaafd9766907
MalicknND <ndiayemalick062@gmail.com>
2025-04-27 00:18:26 +0200

    Réorganisation de la mise en page de la page d'accueil avec une grille responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      {/* <Button size="lg" className="rounded-full text-custom-color">
-        <CirclePlus className="w-4 h-4 mr-2" />
-        Click me
-      </Button> */}
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        Test
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        Test
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
     </div>
   );
 }
